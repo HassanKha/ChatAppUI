00f3a134bb09dfa90d4fd6dc6383108c1c5ec3ff
Hassan Khaled <58123330+HassanKha@users.noreply.github.com>
2023-11-29 23:59:16 +0200

    Add files via upload

--- src/pages/Chatrooms/Rooms.js
+++ src/pages/Chatrooms/Rooms.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import AddCircleOutlinedIcon from "@mui/icons-material/AddCircleOutlined";
 import SettingsIcon from "@mui/icons-material/Settings";
 import SearchIcon from "@mui/icons-material/Search";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { useForm, SubmitHandler } from "react-hook-form";
+
 function rooms() {
   const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const [Addroom, setAddroom] = useState(false);
+  const [UsersAdded, setUsersAdded] = useState([]);
+
+  const onSubmit = (data, e) => {
+    if (e.nativeEvent.submitter.name === "Add") {
+      console.log(data);
+      setUsersAdded([
+        ...UsersAdded,
+        {
+          id: Users.length + UsersAdded.length,
+          email: data.email,
+          name: data.Username,
+          status: false,
+          NotificationNumber: 0,
+          ProfileImg:
+            "https://images.rawpixel.com/image_png_social_square/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTAxL3JtNjA5LXNvbGlkaWNvbi13LTAwMi1wLnBuZw.png",
+        },
+      ]);
+    } else if (e.nativeEvent.submitter.name === "Cancel") {
+      // Logic for button 2
+      setAddroom(false);
+    }
+  };
   const Users = [
     {
       id: 1,
@@ -32,16 +63,29 @@ function rooms() {
         "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png",
     },
   ];
+
+  const AddRoom = () => {
+    setAddroom(true);
+  };
   return (
-    <div className="h-screen flex  flex-col items-center bg-[#8DC26F]">
-      <div className=" h-full w-full">
+    <div
+      className={`   relative h-screen flex  flex-col items-center bg-[#8DC26F]`}
+    >
+      <div
+        className={` ${
+          Addroom && "opacity-10 bg-transparent bg-black"
+        } h-full w-full`}
+      >
         <div className="flex flex-1 pt-4 px-5 justify-between ">
           <h1 className="text-4xl text-center font-serif text-white">
             Chat Application
           </h1>
           <div className="flex gap-2 ">
-            <AddCircleOutlinedIcon className="text-5xl text-white" />
-            <SettingsIcon className="text-5xl text-white" />
+            <AddCircleOutlinedIcon
+              onClick={AddRoom}
+              className="text-5xl text-white cursor-pointer hover:scale-110"
+            />
+            <SettingsIcon className="text-5xl text-white cursor-pointer hover:scale-110" />
           </div>
         </div>
         <div className="p-5">
@@ -85,7 +129,79 @@ function rooms() {
               </div>
             </div>
           ))}
+
+          {UsersAdded.map((user) => (
+            <div
+              onClick={() => router.push(`/Room/${user?.id}`)}
+              key={user?.id}
+              className=" cursor-pointer border-s-2 hover:shadow-sm shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] shadow-black  p-2 flex gap-2 items-center justify-between  bg-[#6F9D55] rounded-sm"
+            >
+              <div className="flex  gap-2">
+                <div className=" h-12 w-12 rounded-full bg-white">
+                  <img
+                    src={user.ProfileImg}
+                    className="  rounded-full object-cover"
+                  />
+                </div>
+
+                <div>
+                  <h1 className="text-white font-bold text-lg">{user.name}</h1>
+                  <p
+                    className={` ${
+                      user.status ? "text-green-300" : "text-orange-400"
+                    }`}
+                  >
+                    {user.status ? "Online" : "Offline"}
+                  </p>
+                </div>
+              </div>
+              <div className="bg-white   h-5 w-5 rounded-full flex items-center justify-center">
+                {user.NotificationNumber}
+              </div>
+            </div>
+          ))}
         </div>
+      </div>
+      <div
+        className={`${
+          !Addroom && "hidden"
+        } shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] shadow-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#6F9D55] p-4`}
+      >
+        <form
+          className=" flex flex-col p-10 gap-8"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            {...register("email", { required: true })}
+            className="outline-none rounded-sm border-white border-2 bg-none border-solid p-2 bg-[#6F9D55] bg-transparent text-gray-50 placeholder-white "
+          />
+          <input
+            type="Username"
+            name="Username"
+            placeholder="Username"
+            {...register("Username", { required: true })}
+            className="outline-none rounded-sm border-white border-2 bg-none border-solid p-2 bg-[#6F9D55] bg-transparent text-gray-50 placeholder-white "
+          />
+
+          <button
+            className="text-white hover:scale-110  border-none shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] shadow-black  bg-[#76b852]  outline-none p-3 rounded-sm cursor-pointer"
+            type="submit"
+            name="Add"
+          >
+            Add Room
+          </button>
+          <button
+            onClick={() => setAddroom(false)}
+            type="submit"
+            name="Cancel"
+            className="text-white hover:scale-110  border-none shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] shadow-black  bg-[#76b852]  outline-none p-3 rounded-sm cursor-pointer"
+          >
+            Cancel
+          </button>
+        </form>
       </div>
     </div>
   );
